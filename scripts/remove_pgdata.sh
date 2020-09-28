24c1b3e83a1ee8961789d95aa92e26e48e503dd7
Emon331046 <emon@appscode.com>
2020-09-28 18:15:21 +0600

    fixing base_backup issue

--- scripts/remove_pgdata.sh
+++ scripts/remove_pgdata.sh
@@ -7,9 +7,9 @@ echo "...............wal position is too old. requesting for base_backup..."
 
 
 # get basebackup
-mkdir -p "$PGDATA"
-rm -rf "$PGDATA"
 chmod 0700 "$PGDATA"
+#mkdir -p "$PGDATA"
+rm -rf "$PGDATA"
 
 
 
