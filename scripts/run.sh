0660e5a31594c6e898c04d8c2fc92e55a07e7202
Emon331046 <emon@appscode.com>
2020-11-08 17:48:25 +0600

    fix: script modified

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,8 +3,8 @@ set -eou pipefail
 
 while true; do
     echo "waiting for the role to be decided ..."
-    if [[ -e /run_scripts/run.sh ]]; then
-        /run_scripts/run.sh && break ;
+    if [[ -e /run_scripts/role/run.sh ]]; then
+        /run_scripts/role/run.sh && break ;
     fi
     sleep 1
 done
