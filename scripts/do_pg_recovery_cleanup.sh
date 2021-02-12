29656e3eb0a10eb196a1f39314638359ba5ebeb8
Emon46 <emon@appscode.com>
2021-02-12 19:34:09 +0600

    fix: password authentication for non-tls
    Signed-off-by: Emon46 <emon@appscode.com>

--- scripts/do_pg_recovery_cleanup.sh
+++ scripts/do_pg_recovery_cleanup.sh
@@ -6,10 +6,8 @@ set -eou pipefail
 mkdir -p "$PGDATA"
 rm -f "$PGDATA"/recovery.conf
 rm -f "$PGDATA"/recovery.done
-if [ "$MAJOR_PG_VERSION" = "9" ]; then
-    export PGWAL="$PGDATA/pg_xlog"
-else
-    export PGWAL="$PGDATA/pg_wal"
-fi
-#mkdir -p "$PGWAL"/archive_status/
-#rm -rf "$PGWAL"/archive_status/*
+
+export PGWAL="$PGDATA/pg_wal"
+
+mkdir -p "$PGWAL"/archive_status/
+rm -rf "$PGWAL"/archive_status/*
