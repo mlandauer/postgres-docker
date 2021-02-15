e9f4dca3d95deb4b6b3e8d52dc0e6d4a5a949376
Emon46 <emon@appscode.com>
2021-02-15 21:32:14 +0600

    fix: typo
    Signed-off-by: Emon46 <emon@appscode.com>

--- scripts/config_recovery.conf.sh
+++ scripts/config_recovery.conf.sh
@@ -8,7 +8,7 @@ echo "recovery_target_timeline = 'latest'" >>/tmp/recovery.conf
 #echo "archive_cleanup_command = 'pg_archivecleanup $PGWAL %r'" >>/tmp/recovery.conf
 
 # primary_conninfo is used for streaming replication
-if [ "${SSL_MODE+0}" = "ON" ]; then
+if [ "${SSL_MODE:-0}" = "ON" ]; then
     echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST sslmode=require sslcert=/tls/certs/client/client.crt sslkey=/tls/certs/client/client.key'" >>/tmp/recovery.conf
 else
     echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST password=$POSTGRES_PASSWORD'" >>/tmp/recovery.conf
