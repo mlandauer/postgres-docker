63d66a95a6088ea6dbdf7212d2b0a0b6f879f571
Emon46 <emon@appscode.com>
2021-02-19 16:28:56 +0600

    update sslmode=$SSL_MODE
    Signed-off-by: Emon46 <emon@appscode.com>

--- scripts/do_pg_basebackup.sh
+++ scripts/do_pg_basebackup.sh
@@ -9,9 +9,8 @@ rm -rf "$PGDATA"/*
 chmod 0700 "$PGDATA"
 echo "attempting pg_basebackup..."
 
-
  if [ "${SSL_MODE:-0}" = "ON" ]; then
-   pg_basebackup -X fetch  --pgdata "$PGDATA" --username=postgres --host="$PRIMARY_HOST" -d "sslmode=require sslcert=/tls/certs/client/client.crt sslkey=/tls/certs/client/client.key" &>/dev/null
+   pg_basebackup -X fetch  --pgdata "$PGDATA" --username=postgres --host="$PRIMARY_HOST" -d "sslmode=$SSL_MODE sslrootcert=/tls/certs/client/ca.crt sslcert=/tls/certs/client/client.crt sslkey=/tls/certs/client/client.key" &>/dev/null
   else
    pg_basebackup -X fetch --no-password --pgdata "$PGDATA" --username=postgres --host="$PRIMARY_HOST" -d "password=$POSTGRES_PASSWORD" &>/dev/null
 fi
