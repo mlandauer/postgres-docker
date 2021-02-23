eff24df043563511abdf964d261b10f6334fea18
Emon46 <emon@appscode.com>
2021-02-23 15:53:46 +0600

    fix: postgres password issue Signed-off-by: Emon46 <emon@appscode.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,10 @@
 set -eou pipefail
 #going to change this with the check of process id
 rm -f "$PGDATA"/postmaster.pid
+echo "waiting for the role to be decided ..."
 while true; do
-    echo "waiting for the role to be decided ..."
     if [[ -e /run_scripts/role/run.sh ]]; then
+        echo "running the initial script ..."
         /run_scripts/role/run.sh && break ;
     fi
     sleep 1
